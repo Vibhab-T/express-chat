5da0fb0ee93370dc3aae9c9bcd59a1a5f9f49c17
Vibhab-T <vibhabtimsina86@gmail.com>
2025-06-05 11:29:54 +0545

    logout functionality

--- frontend/src/components/sidebar/logoutButton.jsx
+++ frontend/src/components/sidebar/logoutButton.jsx
@@ -1,13 +1,21 @@
 import { BiLogOutCircle } from 'react-icons/bi';
+import useLogout from '../../hooks/useLogout';
 
 const Logout = () => {
+	const { loading, logout } = useLogout();
+
 	return (
 		<div className='mt-auto'>
-			<button
-				type='submit'
-				className='btn btn-cirlce bg-transparent text-teal-500 border-green-200 rounded-full'>
-				<BiLogOutCircle className='w-6 h-6 outline-none curson-pointer ' />
-			</button>
+			{!loading ? (
+				<button className='btn btn-cirlce bg-transparent text-teal-500 border-green-200 rounded-full'>
+					<BiLogOutCircle
+						className='w-6 h-6 outline-none curson-pointer '
+						onClick={logout}
+					/>
+				</button>
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
 		</div>
 	);
 };
